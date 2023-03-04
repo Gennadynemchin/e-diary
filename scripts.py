14b40ed3c195b615b194e9551985605f761fbb2d
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-03-05 01:10:08 +0300

    Added scripts.py -> fix_marks()

--- scripts.py
+++ scripts.py
@@ -3,12 +3,19 @@ from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 import random
 
 
-def fix_marks(child):
-    marks = Mark.objects.filter(schoolkid=child)
-    bad_points = marks.filter(points__in=[2, 3])
-    for bad_point in bad_points:
-        bad_point.points = 5
-        bad_point.save()
+def fix_marks(name):
+    year_of_study = 6
+    try:
+        child = Schoolkid.objects.get(full_name__contains=name, year_of_study=year_of_study)
+        marks = Mark.objects.filter(schoolkid=child)
+        bad_points = marks.filter(points__in=[2, 3])
+        for bad_point in bad_points:
+            bad_point.points = 5
+            bad_point.save()
+    except MultipleObjectsReturned:
+        print('Returned more than 1 result. Aborting.')
+    except ObjectDoesNotExist:
+        print('Matching query does not exist. Aborting.')
 
 
 def remove_chastisements(schoolkid):
